54ca7128894fe8bb956df0db5746bb8acff429bd
Gustavo Lobo <gustavoqe.75@gmail.com>
2023-11-07 09:36:30 -0300

    Seeders prontos

--- database/seeders/OrderItemSeeder.php
+++ database/seeders/OrderItemSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Enums\CardStatus;
 use App\Enums\OrderItemsStatus;
+use App\Enums\OrderStatus;
 use App\Models\Card;
 use App\Models\CardPhysical;
+use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
 use App\Models\ProductCategory;
@@ -41,34 +43,197 @@ class OrderItemSeeder extends Seeder
 
         Product::factory([
             'atcm_products_categories_id' => $productCategory->id,
-
+            'name'        => 'Boliche',
+            'description' => '30 minutos de boliche',
+            'time'        => '30',
+            'value'       => '15',
+            'cost'        => '3',
         ])->create();
 
-        ProductCategory::factory([
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Karaôke',
+            'description' => '5 minutos de karaôke',
+            'time'        => '5',
+            'value'       => '0',
+            'cost'        => '0',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Sinuca',
+            'description' => '1 hora de sinuca',
+            'time'        => '60',
+            'value'       => '15',
+            'cost'        => '3',
+        ])->create();
+
+        $productCategory = ProductCategory::factory([
             'description' => 'Hambúrgueres',
         ])->create();
 
-        ProductCategory::factory([
-            'description' => 'Acompanhantes',
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Angus Bacon',
+            'description' => '1x Hambúguer Artesanal 250g; Bacon; Salada; Pão Brioche.',
+            'value'       => '29.90',
+            'cost'        => '13',
         ])->create();
 
-        ProductCategory::factory([
-            'description' => 'Acompanhantes',
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Angus Duplo Bacon',
+            'description' => '2x Hambúguer Artesanal 250g; Bacon; Salada; Pão Brioche.',
+            'value'       => '35.90',
+            'cost'        => '17',
         ])->create();
 
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Angus Cheddar Bacon',
+            'description' => '1x Hambúguer Artesanal 250g; Cheddar; Bacon; Salada; Pão Brioche.',
+            'value'       => '32.90',
+            'cost'        => '15',
+        ])->create();
 
-        // $date = now()->subMonth()->startOfMonth();
+        $productCategory = ProductCategory::factory([
+            'description' => 'Porções',
+        ])->create();
 
-        // for ($i = 0; $i < 30; $i ++) {
-        //     OrderItem::factory(['status' => OrderItemsStatus::Delivered->value, 'created_at' => $date])->count(rand(5, 30))->create();
-        //     $date->addDay();
-        // }
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Batata Frita 500g',
+            'description' => '500 gramas de batata frita.',
+            'value'       => '15.90',
+            'cost'        => '5',
+        ])->create();
 
-        // $date = now()->startOfMonth();
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Batata Frita Bacon Cheddar 500g',
+            'description' => '500 gramas de batata frita + bacon + cheddar.',
+            'value'       => '19.90',
+            'cost'        => '8',
+        ])->create();
 
-        // for ($i = 0; $i <= now()->day; $i ++) {
-        //     OrderItem::factory(['status' => OrderItemsStatus::Delivered->value, 'created_at' => $date])->count(rand(10, 60))->create();
-        //     $date->addDay();
-        // }
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Batata Rústica 500g',
+            'description' => '500 gramas de batata frita rústica.',
+            'value'       => '19.90',
+            'cost'        => '8',
+        ])->create();
+
+        $productCategory = ProductCategory::factory([
+            'description' => 'Sobremesas',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Pudim',
+            'description' => 'Mini Pudim de Copo.',
+            'value'       => '7.90',
+            'cost'        => '3',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Torta de Limão',
+            'description' => '1x pedaço torta de limão.',
+            'value'       => '12.90',
+            'cost'        => '5',
+        ])->create();
+
+        $productCategory = ProductCategory::factory([
+            'description' => 'Bebidas',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Água 300ml (sem gás)',
+            'description' => 'Água Mineral Natural s/ gás. 300ml.',
+            'value'       => '5.90',
+            'cost'        => '1',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Água 300ml (com gás)',
+            'description' => 'Água Mineral Natural c/ gás. 300ml.',
+            'value'       => '5.90',
+            'cost'        => '1',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Coca Cola 350ml',
+            'description' => 'Refrigerante de Cola 350ml',
+            'value'       => '6',
+            'cost'        => '2',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Coca Cola 350ml (Sem Açucar)',
+            'description' => 'Refrigerante de Cola 350ml (Sem Açucar)',
+            'value'       => '6',
+            'cost'        => '2',
+        ])->create();
+
+        Product::factory([
+            'atcm_products_categories_id' => $productCategory->id,
+            'name'        => 'Coca Cola 2l',
+            'description' => 'Refrigerante de Cola 2 litros.',
+            'value'       => '14.50',
+            'cost'        => '6',
+        ])->create();
+
+        $date = now()->subMonth()->startOfMonth();
+
+        for ($i = 0; $i < 30; $i ++) {
+            $order = Order::factory([
+                'atcm_card_id' => Card::find(rand(1, 100)),
+                'status' => OrderStatus::Concluded->value,
+            ])->create();
+
+            for ($z = 0; $z <= 5; $z ++) { 
+                $product = Product::find(rand(1, 16));
+
+                OrderItem::factory([
+                    'atcm_order_id'   => $order->id,
+                    'atcm_product_id' => $product->id,
+                    'value'           => $product->value,
+                    'cost'            => $product->cost,    
+                    'status'          => OrderItemsStatus::Delivered->value, 
+                    'created_at'      => $date
+                ])->create();
+            }
+
+            $date->addDay();
+        }
+
+        $date = now()->startOfMonth();
+
+        for ($i = 0; $i < now()->day; $i ++) {
+            $order = Order::factory([
+                'atcm_card_id' => Card::find(rand(1, 100)),
+                'status' => OrderStatus::Concluded->value,
+            ])->create();
+
+            for ($z = 0; $z <= 8; $z ++) { 
+                $product = Product::find(rand(1, 16));
+
+                OrderItem::factory([
+                    'atcm_order_id'   => $order->id,
+                    'atcm_product_id' => $product->id,
+                    'value'           => $product->value,
+                    'cost'            => $product->cost,
+                    'status'          => OrderItemsStatus::Delivered->value, 
+                    'created_at'      => $date
+                ])->create();
+            }
+            
+            $date->addDay();
+        }
     }
 }
